63fc03f6a033850633369eda6d472e7f84395b66
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 19:45:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const createFareMultiplier=function(n){
 
 const fareDoubler=createFareMultiplier(2)
 const fareTripler=createFareMultiplier(3)
+
+const selectDifferentDrivers=function(drivers,func){
+   return func(drivers)
+}
