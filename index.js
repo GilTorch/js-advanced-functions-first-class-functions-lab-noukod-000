e816d1553543c29f701e66fbb1216853d45f11cb
Gilbert Torchon <gilbert@noukod.com>
2019-02-07 19:42:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers=function(array){
 }
 
 const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=function(n){
+  return function(fare){
+    return fare*n;
+  }
+}
